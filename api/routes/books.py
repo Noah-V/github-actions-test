d5dbeb91e934c8b564d5dc35564e45b24ba3d388
Noah-V <noahvikoo3@gmail.com>
2025-02-13 02:53:06 +0100

    Added extras

--- api/routes/books.py
+++ api/routes/books.py
@@ -56,7 +56,7 @@ async def get_book_by_id(book_id: int) -> Book:
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Book with id {book_id} not found",
         )
-    return book
+    return JSONResponse(status_code=status.HTTP_200_OK, content=book.model_dump())
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
